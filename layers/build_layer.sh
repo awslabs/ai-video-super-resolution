5636472a5aa915c017103ee7c3a4e2da0fac6ae4
xiaoth <xiaotih@amazon.com>
2021-11-23 17:56:26 +0800

    2.0.0

--- layers/build_layer.sh
+++ layers/build_layer.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-wget --no-check-certificate https://xiaotih.s3.us-west-2.amazonaws.com/ffprobe.zip -O $(pwd)/ffprobe.zip
-#docker build -t ffmpeg_layer .
-#docker run --rm -v $(pwd):/data ffmpeg_layer cp /packages/ffprobe.zip /data
-#docker rmi ffmpeg_layer
+
+docker build -t ffmpeg_layer .
+docker run --rm -v $(pwd):/data ffmpeg_layer cp /packages/ffprobe.zip /data
+docker rmi ffmpeg_layer
 
 
 
